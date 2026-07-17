874f2686306788b15e363203b0726e6326f48547
Max Arnautov <amxm@ya.ru>
2026-07-17 17:12:41 +0300

    Add: unique-email-addresses - submission-2

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        res = set()
+
+        for email in emails:
+            local, domain = email.split('@')
+
+            local = local.split('+', 1)[0]
+            local = local.replace('.', '')
+
+            res.add(f'{local}@{domain}')
+
+        return len(res)  
